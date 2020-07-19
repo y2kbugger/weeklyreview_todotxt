09b195a29dc30bf0b1be606ed0f710a861467eb5
y2k <git@y2kbugger.com>
2020-07-19 02:13:52 -0400

    Passed tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -8,3 +8,8 @@ def test_single_char():
 
 def test_completed():
     assert Line("x c").persist() == "x c"
+
+def test_can_uncomplete():
+    l = Line("x c")
+    l.complete(False)
+    l.persist() == "c"
--- weeklyreviewtodotxt/parser.py
+++ weeklyreviewtodotxt/parser.py
@@ -1,5 +1,12 @@
 class Line():
     def __init__(self, line):
         self.line = line
+        self.completed = self.line[0:1] == "x "
+        if self.completed:
+            self.line = self.line[2:]
+
+    def complete(self, completed=True):
+        self.completed = completed
+
     def persist(self):
         return self.line
