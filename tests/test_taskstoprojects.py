aabe585579811c60677b848cca81313a1a4c0c3e
y2k <git@y2kbugger.com>
2020-07-19 22:01:00 -0400

    Refactor module level functions to class

--- tests/test_taskstoprojects.py
+++ tests/test_taskstoprojects.py
@@ -1,16 +1,25 @@
+import pytest
+
 from weeklyreviewtodotxt.parser import Task
-from weeklyreviewtodotxt.tasktoprojects import convert_task_to_project, assign_task_to_project
+from weeklyreviewtodotxt.tasktoprojects import TasksToProjects
 
 from test_parser import assert_tasks_equal
 
-def test_turn_task_into_project():
+@pytest.fixture
+def ttp():
+    ttp = TasksToProjects()
+    return ttp
+
+
+def test_turn_task_into_project(ttp):
     t = Task("earn degree")
-    new_t = convert_task_to_project(t)
+    new_t = ttp.convert_task_to_project(t)
     assert_tasks_equal(t, Task("earn degree"))
     assert_tasks_equal(new_t, Task("prj:earn_degree @@@project"))
 
-def test_can_assign_to_existing_project():
+def test_can_assign_to_existing_project(ttp):
     t = Task("research graduate programs")
-    new_t = assign_task_to_project(t, 'prj:earn_degree')
+    new_t = ttp.assign_task_to_project(t, 'prj:earn_degree')
     assert_tasks_equal(t, Task("research graduate programs"))
     assert_tasks_equal(new_t, Task("research graduate programs prj:earn_degree"))
+
--- weeklyreviewtodotxt/tasktoprojects.py
+++ weeklyreviewtodotxt/tasktoprojects.py
@@ -1,15 +1,16 @@
 from .parser import Task, Generic
 
-def convert_task_to_project(task : Task):
-    first_generic = [p for p in task.parts if type(p) is Generic][0]
-    new_task = Task(task.persist)
-    new_task.remove_part(first_generic.persist)
-    new_task.add_part("prj:" + first_generic.persist.replace(' ', '_'))
-    new_task.add_part("@@@project")
-    return new_task
+class TasksToProjects:
+    def convert_task_to_project(self, task : Task):
+        first_generic = [p for p in task.parts if type(p) is Generic][0]
+        new_task = Task(task.persist)
+        new_task.remove_part(first_generic.persist)
+        new_task.add_part("prj:" + first_generic.persist.replace(' ', '_'))
+        new_task.add_part("@@@project")
+        return new_task
 
-def assign_task_to_project(task : Task, project : str):
-    new_task = Task(task.persist)
-    assert project[:4] == 'prj:'
-    new_task.add_part(project)
-    return new_task
+    def assign_task_to_project(self, task : Task, project : str):
+        new_task = Task(task.persist)
+        assert project[:4] == 'prj:'
+        new_task.add_part(project)
+        return new_task
