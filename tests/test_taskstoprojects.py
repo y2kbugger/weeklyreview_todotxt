143cb532564c7990f730760a9e88c39891ada4da
y2k <git@y2kbugger.com>
2020-07-19 21:50:22 -0400

    Make high level scenario functions non-mutating

--- tests/test_taskstoprojects.py
+++ tests/test_taskstoprojects.py
@@ -5,9 +5,12 @@ from test_parser import assert_tasks_equal
 
 def test_turn_task_into_project():
     t = Task("earn degree")
-    convert_task_to_project(t)
-    assert_tasks_equal(t, Task("prj:earn_degree @@@project"))
+    new_t = convert_task_to_project(t)
+    assert_tasks_equal(t, Task("earn degree"))
+    assert_tasks_equal(new_t, Task("prj:earn_degree @@@project"))
 
-def test_can_assign_to_existing_project():
-    t = Task("research graduate programs")
-    assign_task_to_project(t, 'prj:earn_degree')
+# def test_can_assign_to_existing_project():
+#     t = Task("research graduate programs")
+#     new_t = assign_task_to_project(t, 'prj:earn_degree')
+#     assert_tasks_equal(t, Task("research graduate programs"))
+#     assert_tasks_equal(new_t, Task("research graduate programs prj:earn_degree"))
--- weeklyreviewtodotxt/tasktoprojects.py
+++ weeklyreviewtodotxt/tasktoprojects.py
@@ -2,9 +2,11 @@ from .parser import Task, Generic
 
 def convert_task_to_project(task : Task):
     first_generic = [p for p in task.parts if type(p) is Generic][0]
-    task.remove_part(first_generic.persist)
-    task.add_part("prj:" + first_generic.persist.replace(' ', '_'))
-    task.add_part("@@@project")
+    new_task = Task(task.persist)
+    new_task.remove_part(first_generic.persist)
+    new_task.add_part("prj:" + first_generic.persist.replace(' ', '_'))
+    new_task.add_part("@@@project")
+    return new_task
 
 def assign_task_to_project(task : Task, project : str):
     assert project[:4] == 'prj:'
