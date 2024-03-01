5745523ca568f6ad7df767fd42044a6c9e792850
y2k <git@y2kbugger.com>
2024-03-01 08:43:12 -0500

    Extract db pytest fixture

--- insync/db.test.py
+++ insync/db.test.py
@@ -1,19 +1,26 @@
+from typing import Iterator
+
+import pytest
+
 from insync.db import ListDB
 
 
-def test_instantiate() -> None:
-    db = ListDB(':memory:')
+@pytest.fixture()
+def db() -> Iterator[ListDB]:
+    _db = ListDB(':memory:')
+    _db.ensure_tables_created()
+    yield _db
+    _db.close()
+
+
+def test_instantiate(db: ListDB) -> None:
     assert db is not None
 
 
-def test_ensure_tables_created_is_idempotent() -> None:
-    db = ListDB(':memory:')
-    db.ensure_tables_created()
+def test_prexisting_tables_doesnt_raise(db: ListDB) -> None:
     db.ensure_tables_created()
 
 
-def test_can_retrieve_empty_list() -> None:
-    db = ListDB(':memory:')
-    db.ensure_tables_created()
+def test_can_retrieve_empty_list(db: ListDB) -> None:
     reg = db.load()
     assert len(reg.items) == 0
