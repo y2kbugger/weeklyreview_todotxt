3aa7a4c71b01e1e27b53630f11f941b021805862
y2k <git@y2kbugger.com>
2024-03-01 19:49:13 -0500

    Add adapter and convert for UUIDLE type in sqlite

--- insync/db.py
+++ insync/db.py
@@ -2,19 +2,25 @@ import os
 import sqlite3
 import sys
 
+from uuid6 import UUID
+
 from insync.list import ListItem, ListRegistry
+
+sqlite3.register_adapter(UUID, lambda u: u.bytes_le)
+sqlite3.register_converter('UUIDLE', lambda b: UUID(bytes_le=b))
 
 
 class ListDB:
+
     def __init__(self, db_path: str | os.PathLike):
-        self._conn = sqlite3.connect(db_path)
+        self._conn = sqlite3.connect(db_path, detect_types=sqlite3.PARSE_DECLTYPES)
 
     def ensure_tables_created(self) -> None:
         try:
             self._conn.execute(
                 """
                 CREATE TABLE IF NOT EXISTS list (
-                    uuid TEXT PRIMARY KEY,
+                    uuid UUIDLE PRIMARY KEY,
                     description TEXT,
                     context TEXT,
                     completed INTEGER
@@ -41,13 +47,14 @@ class ListDB:
 
         self._conn.executemany(
             sql,
-            ((str(item.uuid), item.description, str(item.context), item.completed) for item in reg.items),
+            ((UUID(bytes_le=item.uuid.bytes_le), item.description, item.context, item.completed) for item in reg.items),
         )
 
         self._conn.commit()
 
     def load(self) -> ListRegistry:
-        cursor = self._conn.execute("""
+        cursor = self._conn.cursor()
+        cursor = cursor.execute("""
             SELECT
                 uuid,
                 description,
@@ -57,11 +64,12 @@ class ListDB:
             """)
         reg = ListRegistry()
         for row in cursor:
+            print(row[0])
             li = ListItem(
                 uuid=row[0],
                 description=row[1],
                 completed=row[2],
-                context=row[3], #TODO: this is totally the wrong type but looks correct in the the scratch.py
+                context=row[3],  # TODO: this is totally the wrong type but looks correct in the the scratch.py
             )
             reg.add(li)
 
