baa29519f2225f9f49dd535dda0d91784de23a68
y2k <git@y2kbugger.com>
2024-03-11 18:18:45 -0400

    Extract keepalive part of websocket endpoint

--- insync/app/ws.py
+++ insync/app/ws.py
@@ -2,11 +2,18 @@ from typing import Annotated
 
 from fastapi import Depends, WebSocket, WebSocketDisconnect
 
-from insync.db import ListDB
-from insync.listregistry import ListItemProject, ListItemProjectType, ListRegistry
+from insync.listregistry import ListItemProject, ListItemProjectType, null_listitemproject
 
-from . import app, get_db, get_registry, get_ws_list_updater
+from . import app, get_ws_list_updater
 from .ws_list_updater import WebsocketListUpdater
+
+
+async def _ws_keep_alive(ws_list_updater: WebsocketListUpdater, websocket: WebSocket) -> None:
+    try:
+        async for _htmx_json in websocket.iter_text():
+            raise RuntimeError("Message received, but this websocket is mean only to transmit updates.")
+    except WebSocketDisconnect:
+        ws_list_updater.disconnect(websocket)
 
 
 @app.websocket("/ws/{list_project_type}/{list_project_name}")
@@ -15,19 +22,10 @@ async def ws(
     list_project_name: str,
     websocket: WebSocket,
     ws_list_updater: Annotated[WebsocketListUpdater, Depends(get_ws_list_updater)],
-    registry: Annotated[ListRegistry, Depends(get_registry)],
-    db: Annotated[ListDB, Depends(get_db)],
 ) -> None:
-    assert list_project_type == ListItemProjectType.checklist, f"Only {ListItemProjectType.checklist} is supported currently"
     project = ListItemProject(list_project_name, list_project_type)
-    # TODO: add support for other list types
 
     await ws_list_updater.subscribe(websocket, project)
     await ws_list_updater.broadcast_update(project)
-
-    try:
-        async for _htmx_json in websocket.iter_text():
-            raise RuntimeError("Message received, but this websocket is mean only to transmit updates.")
-    except WebSocketDisconnect:
-        ws_list_updater.disconnect(websocket)
+    await _ws_keep_alive(ws_list_updater, websocket)
 
