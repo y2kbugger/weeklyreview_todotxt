ffd3334efc77e99cee46071af07116f2854a6200
y2k <git@y2kbugger.com>
2024-03-22 21:30:09 -0400

    Organize ws_list_update_test

--- insync/app/ws_list_updater_test.py
+++ insync/app/ws_list_updater_test.py
@@ -32,125 +32,126 @@ def renderer() -> MockRenderer:
     return MockRenderer()
 
 
-class MockWebSocket:
-    client_state = WebSocketState.CONNECTED
+class TestRendering:
+    def test_can_render_channel_subscription(
+        self,
+        reg: ListRegistry,
+        updater: WebSocketListUpdater,
+        renderer: MockRenderer,
+    ) -> None:
+        item = ListItem('test1A')
+        reg.add(item)
+        updater.register_channel(item.project, renderer)
 
-    def __init__(self):
-        self.sent = None
-        self.accepted = False
+        result = updater.render_channel(item.project)
 
-    async def accept(self) -> None:
-        self.accepted = True
+        assert len(renderer.calls) == 1
+        assert result == 'test1A'
 
-    async def send_text(self, message: str) -> None:
-        assert self.accepted, "Cannot send text before accepting the connection"
-        assert self.sent is None, "Mock can only can handle a single sent message"
-        self.sent = message
+    def test_when_rendered_only_matched_item_included(
+        self,
+        reg: ListRegistry,
+        updater: WebSocketListUpdater,
+        renderer: MockRenderer,
+    ) -> None:
+        reg.add(ListItem('test'))
+        reg.add(ListItem('testT', project=ListItemProject('travel', ListItemProjectType.checklist)))
+        reg.add(ListItem('testG', project=ListItemProject('grocery', ListItemProjectType.checklist)))
+        project = ListItemProject('grocery', ListItemProjectType.checklist)
+        updater.register_channel(project, renderer)
 
-    def spy_sent_text(self) -> str:
-        assert self.accepted, "Cannot spy on sent text before accepting the connection"
-        assert self.sent is not None, "No text has been sent"
-        return self.sent
+        result = updater.render_channel(project)
 
+        assert len(renderer.calls) == 1
+        assert result == 'testG'
 
+    def test_can_project_channel_gets_all_with_matching_root_project(
+        self,
+        reg: ListRegistry,
+        updater: WebSocketListUpdater,
+        renderer: MockRenderer,
+    ) -> None:
+        reg.add(ListItem('test1A'))
+        reg.add(ListItem('test2C', project=ListItemProject('travel', ListItemProjectType.checklist)))
+        reg.add(ListItem('test2A', project=ListItemProject('grocery', ListItemProjectType.checklist)))
+        reg.add(ListItem('test3A', project=ListItemProject('grocery.produce', ListItemProjectType.checklist)))
+        reg.add(ListItem('test4B', project=ListItemProject('grocery.camping', ListItemProjectType.todo)))
 
-@pytest.fixture
-def ws(anyio_backend: tuple[str, dict[str, Any]]) -> MockWebSocket:
-    return MockWebSocket()
+        project = ListItemProject('grocery', ListItemProjectType.checklist)
+        updater.register_channel(project, renderer)
 
+        result = updater.render_channel(project)
 
-def test_can_render_channel_subscription(
-    reg: ListRegistry,
-    updater: WebSocketListUpdater,
-    renderer: MockRenderer,
-) -> None:
-    item = ListItem('test1A')
-    reg.add(item)
-    updater.register_channel(item.project, renderer)
+        # 2c is excluded because it's a different checklist
+        # 4b is excluded because it's not a checklist
+        assert result == 'test2A,test3A'
 
-    result = updater.render_channel(item.project)
+    def test_projects_can_be_a_subset(
+        self,
+        reg: ListRegistry,
+        updater: WebSocketListUpdater,
+        renderer: MockRenderer,
+    ) -> None:
+        """This was a bug caused by using the raw str contains as the filter method for projects."""
+        reg.add(ListItem('t1', project=ListItemProject('grocery', ListItemProjectType.checklist)))
+        reg.add(ListItem('t2', project=ListItemProject('grocery.produce', ListItemProjectType.checklist)))
+        reg.add(ListItem('t3', project=ListItemProject('gro', ListItemProjectType.checklist)))
+        reg.add(ListItem('xx', project=ListItemProject('gro', ListItemProjectType.todo)))
 
-    assert len(renderer.calls) == 1
-    assert result == 'test1A'
+        project_grocery = ListItemProject('grocery', ListItemProjectType.checklist)
+        updater.register_channel(project_grocery, renderer)
 
+        project_gro = ListItemProject('gro', ListItemProjectType.checklist)
+        updater.register_channel(project_gro, renderer)
 
-def test_when_rendered_only_matched_item_included(
-    reg: ListRegistry,
-    updater: WebSocketListUpdater,
-    renderer: MockRenderer,
-) -> None:
-    reg.add(ListItem('test'))
-    reg.add(ListItem('testT', project=ListItemProject('travel', ListItemProjectType.checklist)))
-    reg.add(ListItem('testG', project=ListItemProject('grocery', ListItemProjectType.checklist)))
-    project = ListItemProject('grocery', ListItemProjectType.checklist)
-    updater.register_channel(project, renderer)
+        # Act
+        result_grocery = updater.render_channel(project_grocery)
+        result_gro = updater.render_channel(project_gro)
 
-    result = updater.render_channel(project)
-
-    assert len(renderer.calls) == 1
-    assert result == 'testG'
-
-
-def test_can_project_channel_gets_all_with_matching_root_project(
-    reg: ListRegistry,
-    updater: WebSocketListUpdater,
-    renderer: MockRenderer,
-) -> None:
-    reg.add(ListItem('test1A'))
-    reg.add(ListItem('test2C', project=ListItemProject('travel', ListItemProjectType.checklist)))
-    reg.add(ListItem('test2A', project=ListItemProject('grocery', ListItemProjectType.checklist)))
-    reg.add(ListItem('test3A', project=ListItemProject('grocery.produce', ListItemProjectType.checklist)))
-    reg.add(ListItem('test4B', project=ListItemProject('grocery.camping', ListItemProjectType.todo)))
-
-    project = ListItemProject('grocery', ListItemProjectType.checklist)
-    updater.register_channel(project, renderer)
-
-    result = updater.render_channel(project)
-
-    # 2c is excluded because it's a different checklist
-    # 4b is excluded because it's not a checklist
-    assert result == 'test2A,test3A'
-
-
-def test_projects_can_be_a_subset(
-    reg: ListRegistry,
-    updater: WebSocketListUpdater,
-    renderer: MockRenderer,
-) -> None:
-    """This was a bug caused by using the raw str contains as the filter method for projects."""
-    reg.add(ListItem('t1', project=ListItemProject('grocery', ListItemProjectType.checklist)))
-    reg.add(ListItem('t2', project=ListItemProject('grocery.produce', ListItemProjectType.checklist)))
-    reg.add(ListItem('t3', project=ListItemProject('gro', ListItemProjectType.checklist)))
-    reg.add(ListItem('xx', project=ListItemProject('gro', ListItemProjectType.todo)))
-
-    project_grocery = ListItemProject('grocery', ListItemProjectType.checklist)
-    updater.register_channel(project_grocery, renderer)
-
-    project_gro = ListItemProject('gro', ListItemProjectType.checklist)
-    updater.register_channel(project_gro, renderer)
-
-    # Act
-    result_grocery = updater.render_channel(project_grocery)
-    result_gro = updater.render_channel(project_gro)
-
-    # Assert
-    assert result_grocery == 't1,t2'
-    assert result_gro == 't3'
+        # Assert
+        assert result_grocery == 't1,t2'
+        assert result_gro == 't3'
 
 
-async def test_broadcast_to_channel(
-    reg: ListRegistry,
-    updater: WebSocketListUpdater,
-    renderer: MockRenderer,
-    ws: MockWebSocket,
-) -> None:
-    item = ListItem('test1A')
-    reg.add(item)
-    _ws: WebSocket = ws  # type: ignore
-    await updater.subscribe(_ws, NullListItemProject(), renderer)
+class TestBroadcasting:
+    class MockWebSocket:
+        client_state = WebSocketState.CONNECTED
 
-    await updater.broadcast_update(NullListItemProject())
-    result = ws.spy_sent_text()
+        def __init__(self):
+            self.sent = None
+            self.accepted = False
 
-    assert len(renderer.calls) == 1
-    assert result == 'test1A'
+        async def accept(self) -> None:
+            self.accepted = True
+
+        async def send_text(self, message: str) -> None:
+            assert self.accepted, "Cannot send text before accepting the connection"
+            assert self.sent is None, "Mock can only can handle a single sent message"
+            self.sent = message
+
+        def spy_sent_text(self) -> str:
+            assert self.accepted, "Cannot spy on sent text before accepting the connection"
+            assert self.sent is not None, "No text has been sent"
+            return self.sent
+
+    @pytest.fixture
+    def ws(self, anyio_backend: tuple[str, dict[str, Any]]) -> MockWebSocket:
+        return self.MockWebSocket()
+
+    async def test_broadcast_to_channel(
+        self,
+        reg: ListRegistry,
+        updater: WebSocketListUpdater,
+        renderer: MockRenderer,
+        ws: MockWebSocket,
+    ) -> None:
+        item = ListItem('test1A')
+        reg.add(item)
+        _ws: WebSocket = ws  # type: ignore
+        await updater.subscribe(_ws, NullListItemProject(), renderer)
+
+        await updater.broadcast_update(NullListItemProject())
+        result = ws.spy_sent_text()
+
+        assert len(renderer.calls) == 1
+        assert result == 'test1A'
