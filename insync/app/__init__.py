d30d5de40fe3a8bed8a87738f9ddac57e6f527e3
y2k <git@y2kbugger.com>
2024-03-31 09:06:26 -0400

    Add static files and overall layout.css

--- insync/app/__init__.py
+++ insync/app/__init__.py
@@ -4,6 +4,7 @@ import os
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from insync.app.ws_list_updater import WebSocketListUpdater
@@ -38,5 +39,18 @@ def get_ws_list_updater() -> WebSocketListUpdater:
 
 app = FastAPI(lifespan=_lifespan, debug=True, title="InSync", version="0.1.0")
 templates = Jinja2Templates(directory="insync/app")
+
+class StaticFilesWithWhitelist(StaticFiles):
+    def __init__(self, directory: str, included_extensions: list[str]):
+        self.included_extensions = included_extensions
+        super().__init__(directory=directory)
+
+    def lookup_path(self, path: str) -> tuple[str, os.stat_result | None]:
+        if not any(path.endswith(ext) for ext in self.included_extensions):
+            raise FileNotFoundError(404, f"File extension must be one of {','.join(self.included_extensions)}, got {path}")
+        return super().lookup_path(path)
+
+
+app.mount("/static", StaticFilesWithWhitelist("insync/app/", ['css', 'js', 'svg', 'png', 'ico', 'css.map']), name='static')
 
 from . import index, ws, checklist, todotxt  # noqa endpoint imports
