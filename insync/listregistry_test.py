8f0f82df9f4c90bb1172261a731f3c06537aa3cf
y2k <git@y2kbugger.com>
2024-04-10 19:18:41 -0400

    Seriously spruce up ListRegistry and Command tests

--- insync/listregistry_test.py
+++ insync/listregistry_test.py
@@ -24,11 +24,13 @@ from insync.listview import ListView
 def item() -> ListItem:
     return ListItem('test')
 
+
 @pytest.fixture
 def reg(item: ListItem) -> ListRegistry:
     _reg = ListRegistry()
     _reg.add(item)
     return _reg
+
 
 def test_registry_can_return_view(reg: ListRegistry) -> None:
     view = reg.search(project=NullListItemProject())
@@ -42,6 +44,7 @@ command_makers = [
     (lambda item: CreateCommand(item.uuid, item), 'CreateCommand'),
     (lambda item: RecurringCommand(item.uuid, True), 'ArchiveCommand'),
 ]
+
 
 @pytest.fixture(params=[cm[0] for cm in command_makers], ids=[cm[1] for cm in command_makers])
 def cmd(request: pytest.FixtureRequest, item: ListItem) -> Command:
@@ -61,26 +64,26 @@ class TestCommandSemantics:
     - double undo (do, undo, undo) AssertionError
     """
 
-    def test_do(self, item: ListItem, reg: ListRegistry, cmd: Command) -> None:
+    def test_do(self, reg: ListRegistry, cmd: Command) -> None:
         cmd.do(reg)
         assert cmd.done
 
-    def test_undo(self, item: ListItem, reg: ListRegistry, cmd: Command) -> None:
+    def test_undo(self, reg: ListRegistry, cmd: Command) -> None:
         cmd.do(reg)
         cmd.undo(reg)
         assert not cmd.done
 
-    def test_undo_before_do(self, item: ListItem, reg: ListRegistry, cmd: Command) -> None:
+    def test_undo_before_do(self, reg: ListRegistry, cmd: Command) -> None:
         with pytest.raises(AssertionError):
             cmd.undo(reg)
 
-    def test_redo(self, item: ListItem, reg: ListRegistry, cmd: Command) -> None:
+    def test_redo(self, reg: ListRegistry, cmd: Command) -> None:
         cmd.do(reg)
         cmd.undo(reg)
         cmd.do(reg)
         assert cmd.done
 
-    def test_double_do(self, item: ListItem, reg: ListRegistry, cmd: Command) -> None:
+    def test_double_do(self, reg: ListRegistry, cmd: Command) -> None:
         cmd.do(reg)
         with pytest.raises(AssertionError):
             cmd.do(reg)
@@ -90,7 +93,7 @@ class TestCommandSemantics:
             cmd.do(reg)
         assert cmd.done
 
-    def test_double_undo(self, item: ListItem, reg: ListRegistry, cmd: Command) -> None:
+    def test_double_undo(self, reg: ListRegistry, cmd: Command) -> None:
         cmd.do(reg)
         cmd.undo(reg)
         with pytest.raises(AssertionError):
@@ -102,10 +105,29 @@ class TestCommandSemantics:
         assert not cmd.done
 
 
-def test_can_complete_item() -> None:
-    reg = ListRegistry()
-    item = ListItem('test')
-    reg.add(item)
+def test_can_create_item_using_command(reg: ListRegistry) -> None:
+    new_item = ListItem('test')
+    old_len = len(reg)
+
+    reg.do(CreateCommand(new_item.uuid, new_item))
+
+    assert len(reg) == old_len + 1
+    assert new_item in reg
+
+
+def test_can_undo_create_item(reg: ListRegistry) -> None:
+    new_item = ListItem('test')
+    old_len = len(reg)
+    reg.do(CreateCommand(new_item.uuid, new_item))
+
+    reg.undo()
+
+    assert len(reg) == old_len
+    assert new_item not in reg
+
+
+def test_can_complete_item(reg: ListRegistry, item: ListItem) -> None:
+    item.completion_datetime = None
     assert not item.completed
 
     reg.do(cc := CompletionCommand(item.uuid, True))
@@ -114,10 +136,8 @@ def test_can_complete_item() -> None:
     assert item.completion_datetime == cc.completion_datetime_new
 
 
-def test_can_undo_completion() -> None:
-    reg = ListRegistry()
-    item = ListItem('test')
-    reg.add(item)
+def test_can_undo_completion(reg: ListRegistry, item: ListItem) -> None:
+    item.completion_datetime = None
     reg.do(CompletionCommand(item.uuid, True))
     assert item.completed
 
@@ -127,10 +147,8 @@ def test_can_undo_completion() -> None:
     assert item.completion_datetime is None
 
 
-def test_can_uncomplete_item() -> None:
-    reg = ListRegistry()
-    item = ListItem('test', completion_datetime=dt.datetime.now(tz=dt.timezone.utc))
-    reg.add(item)
+def test_can_uncomplete_item(reg: ListRegistry, item: ListItem) -> None:
+    item.completion_datetime = dt.datetime.now(tz=dt.timezone.utc)
     assert item.completed
 
     reg.do(CompletionCommand(item.uuid, False))
@@ -138,31 +156,8 @@ def test_can_uncomplete_item() -> None:
     assert not item.completed
 
 
-def test_can_create_item_using_command() -> None:
-    reg = ListRegistry()
-    item = ListItem('test')
-
-    reg.do(CreateCommand(item.uuid, item))
-
-    assert len(reg) == 1
-    assert item in reg
-
-
-def test_can_undo_create_item() -> None:
-    reg = ListRegistry()
-    item = ListItem('test')
-    reg.do(CreateCommand(item.uuid, item))
-
-    reg.undo()
-
-    assert len(reg) == 0
-    assert item not in reg
-
-
-def test_can_archive_item() -> None:
-    reg = ListRegistry()
-    item = ListItem('test')
-    reg.add(item)
+def test_can_archive_item(reg: ListRegistry, item: ListItem) -> None:
+    item.archival_datetime = None
     assert not item.archived
 
     reg.do(ac := ArchiveCommand(item.uuid, True))
@@ -171,10 +166,8 @@ def test_can_archive_item() -> None:
     assert item.archival_datetime == ac.archival_datetime_new
 
 
-def test_can_unarchive_item() -> None:
-    reg = ListRegistry()
-    item = ListItem('test', archival_datetime=dt.datetime.now(tz=dt.timezone.utc))
-    reg.add(item)
+def test_can_unarchive_item(reg: ListRegistry, item: ListItem) -> None:
+    item.archival_datetime = dt.datetime.now(tz=dt.timezone.utc)
     assert item.archived
 
     reg.do(ArchiveCommand(item.uuid, False))
@@ -182,10 +175,7 @@ def test_can_unarchive_item() -> None:
     assert not item.archived
 
 
-def test_can_undo_archival() -> None:
-    reg = ListRegistry()
-    item = ListItem('test')
-    reg.add(item)
+def test_can_undo_archival(reg: ListRegistry, item: ListItem) -> None:
     reg.do(ArchiveCommand(item.uuid, True))
     assert item.archived
 
@@ -195,86 +185,96 @@ def test_can_undo_archival() -> None:
     assert item.archival_datetime is None
 
 
-def test_reset_checklist_archives_completed() -> None:
-    reg = ListRegistry()
-    item1 = ListItem('test1', project=ListItemProject('grocery', ListItemProjectType.checklist))
-    item2 = ListItem('test2', project=ListItemProject('grocery', ListItemProjectType.checklist))
-    reg.add(item1)
-    reg.add(item2)
-    reg.do(CompletionCommand(item1.uuid, True))
+def test_reset_raises_when_not_a_checklist(reg: ListRegistry, item: ListItem) -> None:
+    with pytest.raises(AssertionError):
+        reg.do(ChecklistResetCommand(item.project))
+
+
+def test_reset_checklist_doesnt_archive_active(reg: ListRegistry, item: ListItem) -> None:
+    item.project = ListItemProject('grocery', ListItemProjectType.checklist)
+    item.completion_datetime = None
+    assert not item.archived
+
+    reg.do(ChecklistResetCommand(item.project))
+
+    assert not item.archived
+
+
+def test_reset_checklist_archives_completed(reg: ListRegistry, item: ListItem) -> None:
+    item.project = ListItemProject('grocery', ListItemProjectType.checklist)
+    item.completion_datetime = dt.datetime.now(tz=dt.timezone.utc)
+    assert item.completed
+    assert not item.archived
+
+    reg.do(ChecklistResetCommand(item.project))
+
+    assert item.completed
+    assert item.archived
+
+
+def test_reset_checklist_does_not_affect_other_projects(reg: ListRegistry, item: ListItem) -> None:
+    item.project = ListItemProject('grocery', ListItemProjectType.checklist)
+    item.completion_datetime = dt.datetime.now(tz=dt.timezone.utc)
+    assert not item.archived
+
+    reg.do(ChecklistResetCommand(ListItemProject('travel', ListItemProjectType.checklist)))
+
+    assert not item.archived
+
+
+def test_reset_checklist_resets_subprojects(reg: ListRegistry, item: ListItem) -> None:
+    item.project = ListItemProject('grocery.produce', ListItemProjectType.checklist)
+    item.completion_datetime = dt.datetime.now(tz=dt.timezone.utc)
+    assert not item.archived
 
     reg.do(ChecklistResetCommand(ListItemProject('grocery', ListItemProjectType.checklist)))
 
-    assert item1.archived
-    assert not item2.archived
+    assert item.archived
 
 
-def test_reset_checklist_does_not_affect_other_projects() -> None:
-    reg = ListRegistry()
-    item1 = ListItem('test1', project=ListItemProject('grocery', ListItemProjectType.checklist))
-    item2 = ListItem('test2', project=ListItemProject('grocery', ListItemProjectType.todo))
-    reg.add(item1)
-    reg.add(item2)
-    reg.do(CompletionCommand(item1.uuid, True))
-    reg.do(CompletionCommand(item2.uuid, True))
+def test_reset_checklist_uncompletes_recurring_items_instead_of_archiving(reg: ListRegistry, item: ListItem) -> None:
+    item.project = ListItemProject('grocery', ListItemProjectType.checklist)
+    item.completion_datetime = dt.datetime.now(tz=dt.timezone.utc)
+    item.recurring = True
+    assert item.completed
+    assert not item.archived
 
-    reg.do(ChecklistResetCommand(ListItemProject('grocery', ListItemProjectType.checklist)))
+    reg.do(ChecklistResetCommand(item.project))
 
-    assert item1.archived
-    assert not item2.archived
+    assert not item.completed
+    assert not item.archived
 
 
-def test_reset_checkout_resets_subprojects() -> None:
-    reg = ListRegistry()
-    item1 = ListItem('test1', project=ListItemProject('grocery.produce', ListItemProjectType.checklist))
-    reg.add(item1)
-    reg.do(CompletionCommand(item1.uuid, True))
+def test_can_undo_reset_checklist_archive(reg: ListRegistry, item: ListItem) -> None:
+    item.project = ListItemProject('grocery', ListItemProjectType.checklist)
+    item.completion_datetime = dt.datetime.now(tz=dt.timezone.utc)
 
-    reg.do(ChecklistResetCommand(ListItemProject('grocery', ListItemProjectType.checklist)))
-
-    assert item1.archived
-
-
-def test_reset_checkilist_uncompletes_recurring_items() -> None:
-    reg = ListRegistry()
-    item1 = ListItem('test1', project=ListItemProject('grocery', ListItemProjectType.checklist), recurring=True)
-    reg.add(item1)
-    reg.do(CompletionCommand(item1.uuid, True))
-
-    reg.do(ChecklistResetCommand(ListItemProject('grocery', ListItemProjectType.checklist)))
-
-    assert not item1.completed
-
-
-def test_can_undo_reset_checklist() -> None:
-    reg = ListRegistry()
-    item1 = ListItem('test1', project=ListItemProject('grocery', ListItemProjectType.checklist))
-    item2 = ListItem('test2', project=ListItemProject('grocery', ListItemProjectType.checklist), recurring=True)
-    reg.add(item1)
-    reg.add(item2)
-    reg.do(CompletionCommand(item1.uuid, True))
-    reg.do(CompletionCommand(item2.uuid, True))
-
-    reg.do(ChecklistResetCommand(ListItemProject('grocery', ListItemProjectType.checklist)))
-    assert item1.completed
-    assert item1.archived
-
-    assert not item2.completed
-    assert not item2.archived
+    reg.do(ChecklistResetCommand(item.project))
+    assert item.completed
+    assert item.archived
 
     reg.undo()
 
-    assert item1.completed
-    assert not item1.archived
-
-    assert not item2.archived
-    assert item2.completed
+    assert item.completed
+    assert not item.archived
 
 
-def test_can_mark_item_as_recurring() -> None:
-    reg = ListRegistry()
-    item = ListItem('test')
-    reg.add(item)
+def test_can_undo_reset_checklist_recur(reg: ListRegistry, item: ListItem) -> None:
+    item.project = ListItemProject('grocery', ListItemProjectType.checklist)
+    item.completion_datetime = dt.datetime.now(tz=dt.timezone.utc)
+    item.recurring = True
+
+    reg.do(ChecklistResetCommand(item.project))
+    assert not item.completed
+    assert not item.archived
+
+    reg.undo()
+
+    assert not item.archived
+    assert item.completed
+
+
+def test_can_mark_item_as_recurring(reg: ListRegistry, item: ListItem) -> None:
     assert not item.recurring
 
     reg.do(RecurringCommand(item.uuid, True))
@@ -282,10 +282,8 @@ def test_can_mark_item_as_recurring() -> None:
     assert item.recurring
 
 
-def test_can_unmark_item_as_recurring() -> None:
-    reg = ListRegistry()
-    item = ListItem('test', recurring=True)
-    reg.add(item)
+def test_can_unmark_item_as_recurring(reg: ListRegistry, item: ListItem) -> None:
+    item.recurring = True
     assert item.recurring
 
     reg.do(RecurringCommand(item.uuid, False))
@@ -293,10 +291,7 @@ def test_can_unmark_item_as_recurring() -> None:
     assert not item.recurring
 
 
-def test_can_undo_mark_as_recurring() -> None:
-    reg = ListRegistry()
-    item = ListItem('test')
-    reg.add(item)
+def test_can_undo_mark_as_recurring(reg: ListRegistry, item: ListItem) -> None:
     reg.do(RecurringCommand(item.uuid, True))
     assert item.recurring
 
