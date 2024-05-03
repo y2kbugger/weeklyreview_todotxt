9dca72e9d07fdb517a3dacb7f3657ffdbced0168
y2k <git@y2kbugger.com>
2024-05-02 23:32:24 -0400

    Authorize based on principal ID list

--- insync/app/__init__.py
+++ insync/app/__init__.py
@@ -1,15 +1,21 @@
-from __future__ import annotations
+# from __future__ import annotations
 
 import os
 from contextlib import asynccontextmanager
+from logging import getLogger
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, Header, HTTPException, Request, WebSocket
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_403_FORBIDDEN
 
 from insync.app.ws_list_updater import WebSocketListUpdater
 from insync.db import ListDB
 from insync.listregistry import ListRegistry
+
+logger = getLogger(__name__)
 
 HOT_RELOAD_ENABLED = os.getenv("HOT_RELOAD_ENABLED", "True").lower() == "true"
 hot_reload = None
@@ -51,7 +57,44 @@ def get_ws_list_updater() -> WebSocketListUpdater:
     return app.state.ws_list_updater
 
 
-app = FastAPI(lifespan=_lifespan, debug=True, title="InSync", version="0.1.0")
+class ZAuth:
+    def __init__(self, valid_principal_names: list[str]):
+        self._valid_principal_names = valid_principal_names
+
+    async def __call__(self, x_ms_client_principal_name: Annotated[list[str] | None, Header()] = None) -> None:
+        if len(self._valid_principal_names) == 0:
+            assert x_ms_client_principal_name is None, "Ensure we are not deployed, we don't want open access to prod"
+            logger.info("No valid_principal_names configured, skipping auth because we are not deployed")
+            return
+        if x_ms_client_principal_name is None:
+            logger.info("No x-ms-client-principal-name header, not authenticated")
+            raise HTTPException(
+                status_code=HTTP_403_FORBIDDEN,
+                detail="Not authenticated",
+            )
+        assert len(x_ms_client_principal_name) == 1, "Expected exactly one x-ms-client-principal-name"
+        client_principal_name = x_ms_client_principal_name[0]
+        if client_principal_name.lower() not in self._valid_principal_names:
+            logger.info("Unauthorized client_principal_name %s", client_principal_name)
+            raise HTTPException(
+                status_code=HTTP_401_UNAUTHORIZED,
+                detail="Not authorized",
+            )
+
+        logger.info("authorized client_principal_name %s", client_principal_name)
+
+
+emails = os.environ.get("INSYNC_VALID_PRINCIPAL_NAMES", "").split(",")
+emails = [email.strip().lower() for email in emails if email.strip() != '']
+zauth = ZAuth(valid_principal_names=emails)
+
+app = FastAPI(
+    lifespan=_lifespan,
+    debug=True,
+    title="InSync",
+    version="0.1.0",
+    dependencies=[Depends(zauth)],
+)
 templates = Jinja2Templates(directory="insync/app")
 
 if HOT_RELOAD_ENABLED:
@@ -74,5 +117,6 @@ class StaticFilesWithWhitelist(StaticFiles):
 
 
 app.mount("/static", StaticFilesWithWhitelist("insync/app/", ['css', 'js', 'svg', 'png', 'ico', 'css.map', 'webmanifest']), name='static')
+
 
 from . import index, sqladmin, ws, checklist, todotxt  # noqa endpoint imports
