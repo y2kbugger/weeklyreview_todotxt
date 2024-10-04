1d592c515493786a0720d0da3374f2ea6e77d442
y2k <git@y2kbugger.com>
2024-10-04 09:15:11 -0400

    Redirect from login if already authenticated

--- insync/app/auth_middleware.py
+++ insync/app/auth_middleware.py
@@ -32,30 +32,44 @@ class AuthMiddleware:
         if calltype != "http" and calltype != "websocket":
             await self.app(scope, receive, send)
             return
+
         path = scope.get("path")
-        if path == "/login":
-            await self.app(scope, receive, send)
-            return
+
+        # try to get token and authenticate user
         cookies = self._get_cookies_from_scope(scope)
         insyncauthn = cookies.get("insyncauthn")
-        if insyncauthn is None:
-            logger.warn("No insyncauthn cookie")
-            response = RedirectResponse(url="/login", status_code=302)
-            await response(scope, receive, send)
-            return
-        for auth in AUTHS:
-            if insyncauthn == auth[1]:
-                user = auth[0]
-                break
-        else:
-            user = None
+        user = None
+        if insyncauthn is not None:
+            for u, tok in AUTHS:
+                if insyncauthn == tok:
+                    user = u
+                    break
 
-        if user is None:
-            logger.warn(f"insyncauthn doesn't match an account {insyncauthn}")
-            response = RedirectResponse(url="/login", status_code=302)
-            await response(scope, receive, send)
-            return
-        else:
-            scope["user"] = user
+        logger.warning(f"{path=}, {insyncauthn=}, {user=}")
+        match (path, insyncauthn, user):
+            case (_, None, None):
+                logger.warning("No insyncauthn cookie")
+                asgi_next = RedirectResponse(url="/login", status_code=302)
+            case (_, _, None):
+                logger.warning(f"insyncauthn doesn't match an account {insyncauthn}")
+                asgi_next = RedirectResponse(url="/login", status_code=302)
+            case ("/login", _, user):
+                # hitting login page, but already logged in
+                logger.warning(f"hitting login page but already logged in {user}")
+                headers = dict(scope.get("headers", []))
+                referer = headers.get(b"referer", b"/")
+                asgi_next = RedirectResponse(url=referer.decode("utf-8"), status_code=302)
+            case ("/login", _, _):
+                logger.warning("Hitting login page, and not logged in")
+                # login page is the only page that can be accessed without a token
+                asgi_next = self.app
+            case (_, _, user):
+                logger.warning(f"user is authenticated {user}")
+                # user is authenticated
+                assert user is not None, "User should not be None here, just in case a mistaken code change in the future might break this guarentee."
+                scope["user"] = user
+                asgi_next = self.app
+            case _:
+                raise NotImplementedError(f"Unhandled case {path=}, {insyncauthn=}, {user=}")
 
-        await self.app(scope, receive, send)
+        await asgi_next(scope, receive, send)
