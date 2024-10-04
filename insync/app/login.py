eea23b9385f56b212d6434678dd975c70ceb999f
y2k <git@y2kbugger.com>
2024-10-04 13:10:31 -0400

    Switch to scriptless login

--- insync/app/login.py
+++ insync/app/login.py
@@ -1,9 +1,33 @@
-from fastapi import Request
-from fastapi.responses import HTMLResponse
+from typing import Annotated
+
+from fastapi import Form, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
 
 from . import app, templates
 
 
-@app.get("/login", response_class=HTMLResponse)
-def get_sqladmin(request: Request) -> HTMLResponse:
+@app.get("/login")
+def get_login(request: Request) -> HTMLResponse:
     return templates.TemplateResponse(request, "login.html", {})
+
+
+@app.post("/login")
+def post_login(token: Annotated[str, Form()]) -> RedirectResponse:
+    response = RedirectResponse("/login", status_code=302)
+    response.set_cookie(
+        key="insyncauthn",
+        value=token,
+        max_age=400 * 24 * 60 * 60,  # 400 days
+        path="/",
+        secure=True,
+        httponly=True,
+        samesite="strict",
+    )
+    return response
+
+
+@app.get("/logout")
+def logout() -> RedirectResponse:
+    response = RedirectResponse("/login", status_code=302)
+    response.delete_cookie(key="insyncauthn")
+    return response
